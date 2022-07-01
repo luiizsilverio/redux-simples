ebb983e611fdbf4636ae25140ee07547b30cc8a5
luiizsilverio <luiiz.silverio@gmail.com>
2022-07-01 19:46:54 -0300

    ajustezinhos

--- src/store/appReducer.js
+++ src/store/appReducer.js
@@ -3,17 +3,20 @@ const INITIAL_STATE = {
   vlMaximo: 10
 }
 
-function appReducer(state = INITIAL_STATE, action) {
+const ATUALIZA_MIN = 'ATUALIZA_MIN'
+const ATUALIZA_MAX = 'ATUALIZA_MAX'
+
+export default function appReducer(state = INITIAL_STATE, action) {
   switch (action.type) {
     case 'LIMPA':
       return {...INITIAL_STATE}
-    case 'ATUALIZA_MIN':
+    case ATUALIZA_MIN:
       return {...state, vlMinimo: action.vlMinimo}
-    case 'ATUALIZA_MAX':
+    case ATUALIZA_MAX:
       return {...state, vlMaximo: action.vlMaximo}
     default:
       return state
   }
 }
 
-export default appReducer
+export { ATUALIZA_MIN, ATUALIZA_MAX }
